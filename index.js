e3e16a3e2a91cc062833dfc8dd3c7b62f0b8920f
Dumitru Cantea <canteadumitru@gmail.com>
2015-10-08 01:44:32 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Promise = require('bluebird');
+var Logger = require('./lib/logger');
+var time = require('time');
+
+
+module.exports = {
+	util: require('./lib/util'),
+	html: require('./lib/html'),
+	text: require('./lib/text'),
+
+	_: require('lodash'),
+	Promise: Promise,
+
+	MemoryCache: require('./lib/MemoryCache'),
+	fs: Promise.promisifyAll(require('fs-extra')),
+
+	//logger
+	Logger: Logger,
+	logger: Logger.logger,
+
+	errors: require('./lib/errors.js'),
+
+	constants: require('./lib/constants.js'),
+
+	crypto: require('./lib/crypto.js'),
+
+	date: function(milliseconds, timezone) {
+		if (typeof milliseconds === 'string') {
+			timezone = milliseconds;
+			milliseconds = undefined;
+		}
+		var date;
+
+		date = milliseconds ? new time.Date(milliseconds) : new time.Date();
+
+		if (timezone) {
+			date.setTimezone(timezone);
+		}
+
+		return date;
+	}
+};
